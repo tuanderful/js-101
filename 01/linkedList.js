cd4d1ae44f30fe7622720827baa5e03ff7506265
Tuan Huynh <tuanlh@gmail.com>
2012-11-12 01:14:52 -0500

    moved some files around, experimented with inheritance

---
+++ 01/linkedList.js
@@ -0,0 +1,23 @@
+/*var LinkedList = (function(){
+	
+	//private variables
+	var root = 10;
+	
+	return {
+		add: function(){
+			console.log('hi');
+		},
+		init: function(){
+			console.log(root);
+		}
+	};
+}());*/
+
+var LinkedList = function(){
+	return {
+		add: function(){
+			console.log('add invoked');
+			
+		}
+	};
+};
