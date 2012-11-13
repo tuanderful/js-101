0b34bead9d612d7c4a3b4d3163b262ac8720a1bf
Tuan Huynh <tuanlh@gmail.com>
2012-11-12 22:31:34 -0500

    get a working example of a constructor

--- 01/linkedList.js
+++ 01/linkedList.js
@@ -1,5 +1,7 @@
 /*
  * Two patterns for creating a constructor with private members
+ * however, in these two examples, I couldn't modify the prototype of LinkedList.
+ * That's because LinkedList was not a constructor, but the object that's returned.
  * 
 
 function LinkedList(){
@@ -23,13 +25,19 @@ function LinkedList(){
 	}
 };*/
 
-var LinkedList = function(){
+
+function LinkedList() {
 	//private variables
 	var _root = 10;
-	
-	return {
-		getFoo: function(){
-			return _root;
-		}
+
+	//public method
+	this.getFoo = function () {
+		return _root;
 	};
+}
+
+var linkedList = new LinkedList();
+
+LinkedList.prototype.getBar = function (price) {
+	console.log("hi");
 };
